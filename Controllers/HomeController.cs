2feb8ae71c6219eaa99d80e13628ca68b247b2e8
kcblast <35716025+kcblast@users.noreply.github.com>
2019-09-13 14:44:35 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TP.Models;
+
+namespace TP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Blog()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> about()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> models()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public async Task<IActionResult> modelsingle()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
